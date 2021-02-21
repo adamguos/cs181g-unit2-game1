6b2ae195cfbf8bf9e5b90d31b3d924419550c3f2
Adam Guo <agsmguo@gmail.com>
2021-02-21 22:01:10 +0800

    Initialise animation lab framework

---
+++ README.md
@@ -0,0 +1,3 @@
+CSCI 181G unit 2 game 1
+
+Adam Guo, Ziang Xue, Gleb Tebaykin
---
+++ src/sprite.rs
@@ -0,0 +1,33 @@
+use crate::texture::Texture;
+use crate::types::{Rect, Vec2i};
+use std::rc::Rc;
+
+pub struct Sprite {
+    image: Rc<Texture>,
+    pub frame: Rect, // Maybe better to use a type that can't have a negative origin
+    // Or use =animation:Animation= instead of a frame field
+    pub position: Vec2i,
+}
+
+impl Sprite {
+    pub fn new(image: &Rc<Texture>, frame: Rect, position: Vec2i) -> Self {
+        Self {
+            image: Rc::clone(image),
+            frame,
+            position,
+        }
+    }
+}
+
+pub trait DrawSpriteExt {
+    fn draw_sprite(&mut self, s: &Sprite);
+}
+
+use crate::screen::Screen;
+impl<'fb> DrawSpriteExt for Screen<'fb> {
+    fn draw_sprite(&mut self, s: &Sprite) {
+        // This works because we're only using a public method of Screen here,
+        // and the private fields of sprite are visible inside this module
+        self.bitblt(&s.image, s.frame, s.position);
+    }
+}
