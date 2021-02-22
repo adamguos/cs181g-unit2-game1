89c90b39bc2088d24f630955741a8f96438c5017
ZiangXue <ziangxue@outlook.com>
2021-02-22 12:55:24 +0800

    Modified collision struct types & gather_contacts

--- src/collision.rs
+++ src/collision.rs
@@ -1,1 +1,226 @@
+//use alloc::collections;
+//use core::f32;
+use pixels::{Pixels, SurfaceTexture};
+use std::{time::Instant, usize};
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
 
+// seconds per frame
+const DT: f64 = 1.0 / 60.0;
+
+const DEPTH: usize = 4;
+const WIDTH: usize = 512;
+const HEIGHT: usize = 480;
+const PITCH: usize = WIDTH * DEPTH;
+
+// We'll make our Color type an RGBA8888 pixel.
+type Color = [u8; DEPTH];
+
+const CLEAR_COL: Color = [32, 32, 64, 255];
+const WALL_COL: Color = [200, 200, 200, 255];
+const PLAYER_COL: Color = [255, 128, 128, 255];
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+enum ColliderID {
+    Terrain(usize),
+    Mobile(usize),
+    Projectile(usize),
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+struct Contact {
+    a: ColliderID,
+    b: ColliderID,
+    mtv: (i32, i32),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Rect {
+    x: i32,
+    y: i32,
+    w: u16,
+    h: u16,
+}
+
+/*  I think we will be doing level generations, so "mobile" will have the
+    ability to move both vertically and horizontally (but for now I assume that
+    the player can move horizontally and the enemy can move vetically and
+    horizontally.) The terrain will move horizontally as they are generated and
+    as the player "advances"
+*/
+
+/*
+   We will mostly be treating terrain as blocks, possibly in rectangle shapes to simplify. It does not need a speed. If with generations it has to move we can constantly change its position based on frame changes.
+*/
+struct Terrain {
+    rect: Rect,
+}
+
+/*
+   Mobiles would need to be able to move freely. We would require its hitbox to be rect.
+*/
+struct Mobile {
+    rect: Rect,
+    vx: i32,
+    vy: i32,
+}
+
+/*
+    Projectiles can cross each others and they will only collide with terrains and mobiles. Since we might need it to point clearly the speed should be floats. (subject to change.)
+*/
+struct Projectile {
+    rect: Rect,
+    vx: f64,
+    vy: f64,
+}
+
+// pixels gives us an rgba8888 framebuffer
+fn clear(fb: &mut [u8], c: Color) {
+    // Four bytes per pixel; chunks_exact_mut gives an iterator over 4-element slices.
+    // So this way we can use copy_from_slice to copy our color slice into px very quickly.
+    for px in fb.chunks_exact_mut(4) {
+        px.copy_from_slice(&c);
+    }
+}
+
+#[allow(dead_code)]
+fn rect(fb: &mut [u8], r: Rect, c: Color) {
+    assert!(r.x < WIDTH as i32);
+    assert!(r.y < HEIGHT as i32);
+    // NOTE, very fragile! will break for out of bounds rects!  See next week for the fix.
+    let x1 = (r.x + r.w as i32).min(WIDTH as i32) as usize;
+    let y1 = (r.y + r.h as i32).min(HEIGHT as i32) as usize;
+    for row in fb[(r.y as usize * PITCH)..(y1 * PITCH)].chunks_exact_mut(PITCH) {
+        for p in row[(r.x as usize * DEPTH)..(x1 * DEPTH)].chunks_exact_mut(DEPTH) {
+            p.copy_from_slice(&c);
+        }
+    }
+}
+
+// Here we will be using push() on into, so it can't be a slice
+fn gather_contacts(
+    terrains: &[Terrain],
+    mobiles: &[Mobile],
+    projs: &[Projectile],
+    into: &mut Vec<Contact>,
+) {
+    // collide mobiles against mobiles
+    for (ai, a) in mobiles.iter().enumerate() {
+        for (bi, b) in mobiles.iter().enumerate().skip(ai + 1) {
+            if !separating_axis(
+                a.rect.x,
+                a.rect.x + a.rect.w as i32,
+                b.rect.x,
+                b.rect.x + b.rect.w as i32,
+            ) && !separating_axis(
+                a.rect.y,
+                a.rect.y + a.rect.h as i32,
+                b.rect.y,
+                b.rect.y + b.rect.h as i32,
+            ) {
+                let contact = Contact {
+                    a: ColliderID::Mobile(ai),
+                    b: ColliderID::Mobile(bi),
+                    mtv: (0, 0),
+                };
+
+                into.push(contact);
+            }
+        }
+    }
+    // collide mobiles against terrains
+    for (ai, a) in mobiles.iter().enumerate() {
+        for (bi, b) in terrains.iter().enumerate() {
+            if !separating_axis(
+                a.rect.x,
+                a.rect.x + a.rect.w as i32,
+                b.rect.x,
+                b.rect.x + b.rect.w as i32,
+            ) && !separating_axis(
+                a.rect.y,
+                a.rect.y + a.rect.h as i32,
+                b.rect.y,
+                b.rect.y + b.rect.h as i32,
+            ) {
+                let contact = Contact {
+                    a: ColliderID::Mobile(ai),
+                    b: ColliderID::Terrain(bi),
+                    mtv: (0, 0),
+                };
+
+                into.push(contact);
+            }
+        }
+    }
+    // collide projs against mobiles
+    for (ai, a) in projs.iter().enumerate() {
+        for (bi, b) in mobiles.iter().enumerate() {
+            if !separating_axis(
+                a.rect.x,
+                a.rect.x + a.rect.w as i32,
+                b.rect.x,
+                b.rect.x + b.rect.w as i32,
+            ) && !separating_axis(
+                a.rect.y,
+                a.rect.y + a.rect.h as i32,
+                b.rect.y,
+                b.rect.y + b.rect.h as i32,
+            ) {
+                let contact = Contact {
+                    a: ColliderID::Projectile(ai),
+                    b: ColliderID::Mobile(bi),
+                    mtv: (0, 0),
+                };
+
+                into.push(contact);
+            }
+        }
+    }
+    // collide projs against terrains
+    for (ai, a) in projs.iter().enumerate() {
+        for (bi, b) in terrains.iter().enumerate() {
+            if !separating_axis(
+                a.rect.x,
+                a.rect.x + a.rect.w as i32,
+                b.rect.x,
+                b.rect.x + b.rect.w as i32,
+            ) && !separating_axis(
+                a.rect.y,
+                a.rect.y + a.rect.h as i32,
+                b.rect.y,
+                b.rect.y + b.rect.h as i32,
+            ) {
+                let contact = Contact {
+                    a: ColliderID::Projectile(ai),
+                    b: ColliderID::Terrain(bi),
+                    mtv: (0, 0),
+                };
+
+                into.push(contact);
+            }
+        }
+    }
+}
+
+fn restitute(statics: &[Terrain], dynamics: &mut [Mobile], contacts: &mut [Contact]) {
+    // handle restitution of dynamics against dynamics and dynamics against statics wrt contacts.
+    // You could instead make contacts `Vec<Contact>` if you think you might remove contacts.
+    // You could also add an additional parameter, a slice or vec representing how far we've displaced each dynamic, to avoid allocations if you track a vec of how far things have been moved.
+    // You might also want to pass in another &mut Vec<Contact> to be filled in with "real" touches that actually happened.
+    contacts.sort_unstable_by_key(|c| -(c.mtv.0 * c.mtv.0 + c.mtv.1 * c.mtv.1));
+    // Keep going!  Note that you can assume every contact has a dynamic object in .a.
+    // You might decide to tweak the interface of this function to separately take dynamic-static and dynamic-dynamic contacts, to avoid a branch inside of the response calculation.
+    // Or, you might decide to calculate signed mtvs taking direction into account instead of the unsigned displacements from rect_displacement up above.  Or calculate one MTV per involved entity, then apply displacements to both objects during restitution (sorting by the max or the sum of their magnitudes)
+}
+
+fn separating_axis(ax1: i32, ax2: i32, bx1: i32, bx2: i32) -> bool {
+    assert!(ax1 <= ax2 && bx1 <= bx2);
+    if ax2 < bx1 || bx2 < ax1 {
+        return true;
+    } else {
+        return false;
+    };
+}
