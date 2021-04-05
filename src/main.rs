bfffbd62f8b664ce44c25e5d20576295a8452fcc
ZiangXue <ziangxue@outlook.com>
2021-04-06 00:05:55 +0800

    changed name to space shooter

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ fn main() {
     let window = {
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
         WindowBuilder::new()
-            .with_title("Unit2Game")
+            .with_title("Space Shooter")
             .with_inner_size(size)
             .with_min_inner_size(size)
             .with_resizable(false)
